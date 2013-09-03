d4c31a99b78554d1839920104559ede6b26382b5
DCatcher <daniel.catchpole@live.com>
2013-09-03 09:45:09 +0100

    Commit Commit

--- Possesion/src/net/dcatcher/possession/client/ClientProxy.java
+++ Possesion/src/net/dcatcher/possession/client/ClientProxy.java
@@ -14,5 +14,6 @@ public class ClientProxy extends CommonProxy {
     public void registerRendering(){
         RenderingRegistry.registerEntityRenderingHandler(EntityHuman.class, new RenderHuman());
 
+
     }
 }
